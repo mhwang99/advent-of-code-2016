95b48b67699f0ff387cdde1d38c039be7b222ce7
Mark Hwang <mhwang@liveopscloud.com>
2016-12-06 09:32:01 -0400

    day 6

--- day6.clj
+++ day6.clj
@@ -1,4 +1,4 @@
-(defn getf
+(defn getmap
   [m e]
   (loop [i 0 e e m m]
     (if (empty? e) m
@@ -7,28 +7,23 @@
           (recur (inc i) (rest e) (update-in m [i f] inc))
           (recur (inc i) (rest e) (assoc-in m [i f] 1)))))))
 
-(defn getmostf
-  [m]
+(defn getone
+  [f m]
   (map #(->> %
              last
              (sort-by last)
-             last
-             first)
-       (sort-by first m)))
-
-(defn getleastf
-  [m]
-  (map #(->> %
-             last
-             (sort-by last)
-             first
+             f
              first)
        (sort-by first m)))
 
 
 (defn day6
   [f l]
-  (apply str (f (reduce #(getf %1 %2) {} l))))
+  (apply str (getone f (reduce #(getmap %1 %2) {} l))))
 
-(day6 getmostf (clojure.string/split (slurp "day6.txt") #"\s+"))
+;part 1
+(day6 last (clojure.string/split (slurp "day6.txt") #"\s+"))
+
+;part 2
+(day6 first (clojure.string/split (slurp "day6.txt") #"\s+"))
 
