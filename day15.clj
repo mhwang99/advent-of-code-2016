d9576edc5a2554f42e0bf47c22b752ef23e83a6e
Mark Hwang <mhwang@liveopscloud.com>
2016-12-15 10:02:59 -0400

    day15

---
+++ day15.clj
@@ -0,0 +1,19 @@
+(defn proc
+  [has start]
+  (loop [n 1]
+    (let [p (->> start
+                (map + (range))
+                (map (partial + n))
+                (map #(mod %2 %1) has))]
+      (if (apply = 0 p) (dec n)
+        (recur (inc n))))))
+
+;part1
+(proc
+[17 7 19 5 3 13]
+[ 1 0  2 0 0 5])
+
+;part2
+(proc
+[17 7 19 5 3 13 11]
+[ 1 0  2 0 0  5  0])
